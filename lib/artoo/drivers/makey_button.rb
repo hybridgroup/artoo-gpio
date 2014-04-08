4de1fcc528ab9b48aec769de65b3c79f1dff3bc4
deadprogram <ron@hybridgroup.com>
2014-04-08 11:52:11 -0700

    Log makey

--- lib/artoo/drivers/makey_button.rb
+++ lib/artoo/drivers/makey_button.rb
@@ -27,6 +27,7 @@ module Artoo
 
         every(100) do
           new_value = connection.digital_read(pin)
+          puts new_value
           @data << new_value
           @data.shift
           update(new_value) if !new_value.nil? && new_value != is_pressed?
@@ -38,11 +39,11 @@ module Artoo
       private
       # Publishes events according to the button feedback
       def update
-        if average_data > 0.5 and not is_pressed?
+        if average_data <= 0.5 and not is_pressed?
           @pressed_val = 1
           publish(event_topic_name("update"), "push", new_val)
           publish(event_topic_name("push"), new_val)
-        elsif average_data <= 0.5 and is_pressed?
+        elsif average_data > 0.5 and is_pressed?
           @pressed_val = 0
           publish(event_topic_name("update"), "release", new_val)
           publish(event_topic_name("release"), new_val)
